84f1bf3e281e3f683511a9f7cab2debf2bba8da8
Wayne Logan <loganamollo@gmail.com>
2024-09-01 13:04:33 +0300

    updates resume

--- src/pages/resume.tsx
+++ src/pages/resume.tsx
@@ -3,10 +3,10 @@ import Head from "next/head";
 import Link from "next/link";
 import Image from "next/image";
 import { FaGithub, FaLinkedinIn, FaDownload } from "react-icons/fa";
-import ResumeShot from "public/assets/Nosarumen_Imuentinyan_Resume.jpg";
+import ResumeShot from "public/assets/resume-image.png";
 
 const ResumePdf = () => {
-	const resumePdfLink = "/assets/Nosarumen_Imuentinyan_Resume.pdf";
+	const resumePdfLink = "/assets/resume-wayne-logan.pdf";
 	return (
 		<>
 			<div className="relative">
@@ -27,10 +27,10 @@ const Resume = () => {
 	return (
 		<>
 			<Head>
-				<title>Nosarumen | Resume</title>
+				<title>Wayne Logan | Resume</title>
 				<meta
 					name="description"
-					content="I’m a front-end web developer specialized in usinf react, nextjs and typescript to build exceptional user interface."
+					content="I'm a full-stack web developer specialized in using React, Typescript and Figma to create visually rich user interfaces."
 				/>
 				<link rel="icon" href="/fav.ico" />
 			</Head>
@@ -38,16 +38,16 @@ const Resume = () => {
 			<div className="max-w-[940px] mx-auto p-2 pt-[120px]">
 				<h2 className="text-center">Resume</h2>
 				<div className="bg-[#d0d4d6] my-4 p-4 w-full flex justify-between items-center">
-					<h2 className="text-center">Nosarumen Imuentinyan</h2>
+					<h2 className="text-center">Wayne Logan</h2>
 					<div className="flex">
 						<a
-							href="https://www.linkedin.com/in/nosarumen-imuentinyan/"
+							href="https://www.linkedin.com/in/wayne-logan/"
 							target="_blank"
 							rel="noreferrer"
 						>
 							<FaLinkedinIn title="Linkedin" size={20} style={{ marginRight: "1rem" }} />
 						</a>
-						<a href="https://github.com/Sir-Rumeh/" target="_blank" rel="noreferrer">
+						<a href="https://github.com/waynelogan/" target="_blank" rel="noreferrer">
 							<FaGithub title="GitHub" size={20} style={{ marginRight: "1rem" }} />
 						</a>
 					</div>
@@ -55,8 +55,8 @@ const Resume = () => {
 				<div className="text-center py-4 text-xl font-bold uppercase tracking-wider">
 					<div className="hidden sm:block">
 						<p>
-							Team Player <span className="px-1">|</span> Web Development{" "}
-							<span className="px-1">|</span> Analytics & Problem Solving
+							Web Development <span className="px-1">|</span> UI/UX Design{" "}
+							<span className="px-1">|</span> Agile
 						</p>
 					</div>
 					<div className="block sm:hidden">
